adb8d86f920eaf0d6136612d80cbd4d5d64a5449
JokerTheBond <48289911+JokerTheBond@users.noreply.github.com>
2023-12-13 23:51:00 +0100

    Fix getAvailableLiquidity in LendingVaultWatcher

--- contracts/LendingVaultWatcher01.sol
+++ contracts/LendingVaultWatcher01.sol
@@ -3,22 +3,25 @@ pragma solidity =0.5.16;
 import "./interfaces/ILendingVaultV1.sol";
 import "./interfaces/IERC20.sol";
 import "./libraries/SafeMath.sol";
+import "./libraries/Math.sol";
 import "./libraries/BorrowableObject.sol";
 
 contract LendingVaultWatcher01 {	
 	using SafeMath for uint;
 	using BorrowableObject for BorrowableObject.Borrowable;
 
-	function getAvailableLiquidity(address vault) external view returns (uint availableLiquidity) {
+	function getAvailableLiquidity(address vault) external returns (uint availableLiquidity) {
 		uint borrowablesLength = ILendingVaultV1(vault).getBorrowablesLength();
 		address underlying = ILendingVaultV1(vault).underlying();
 		for (uint i = 0; i < borrowablesLength; i++) {
 			address borrowable = ILendingVaultV1(vault).borrowables(i);
-			availableLiquidity += IERC20(underlying).balanceOf(borrowable);
+			BorrowableObject.Borrowable memory borrowableObject = BorrowableObject.newBorrowable(IBorrowable(borrowable), vault);
+			uint suppliedAmount = borrowableObject.ownedSupply;
+			uint liquidity = IERC20(underlying).balanceOf(borrowable);
+			availableLiquidity += Math.min(suppliedAmount, liquidity);
 		}
 	}
 
-	// TODO L'ho testato col subgraph e non mi torna...
 	function getSupplyRate(address vault) external returns (uint supplyRate) {
 		uint borrowablesLength = ILendingVaultV1(vault).getBorrowablesLength();
 		uint reserveFactor = ILendingVaultV1(vault).reserveFactor();
@@ -33,9 +36,5 @@ contract LendingVaultWatcher01 {
 			totalProfitsPerSecond += borrowableRate * suppliedAmount;
 		}
 		return totalProfitsPerSecond / totalSuppliedAmount * (uint(1e18).sub(reserveFactor)) / 1e18;
-	}
-
-	function getOptimizedSupplyRate(address vault) external returns (uint supplyRate) {
-		// TODO
 	}
 }
