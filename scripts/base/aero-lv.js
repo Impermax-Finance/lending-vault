6fbf77c85aa899bc0aa51e7d94caff2a001b0fbd
0xl2 <abe.sinjiro7@gmail.com>
2024-07-08 13:03:40 +1000

    feat: update for aero

--- scripts/base/aero-lv.js
+++ scripts/base/aero-lv.js
@@ -4,15 +4,15 @@ const reserveAdmin = "0x9fd93712400902bff6040efa72B28Bf80152F056"
 // const ownerAddr = "0x5ec869c1cb378bb77bc55bb56129399f6828c8c5"
 
 const underlyings = [
-  // {
-  //   ind: 0,
-  //   address: "0x4200000000000000000000000000000000000006", // WETH
-  //   symbol: "ibexETH",
-  //   name: "Impermax Lent ETH",
-  //   borrowables: [
-  //     "0x7fbb6111eAC6e4CB5F5cFfbaa39903Ff4340FF59", // usdc/eth
-  //   ]
-  // },
+  {
+    ind: 0,
+    address: "0x4200000000000000000000000000000000000006", // WETH
+    symbol: "ibexETH",
+    name: "Impermax Lent ETH",
+    borrowables: [
+      "0x7fbb6111eAC6e4CB5F5cFfbaa39903Ff4340FF59", // usdc/eth
+    ]
+  },
   {
     ind: 1,
     address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913", // USDC
@@ -29,24 +29,24 @@ async function main() {
   const [owner] = await ethers.getSigners();
   console.log(owner.address);
 
-  // // deploy LVDeployer
-  // const LVDeployerV1 = await ethers.getContractFactory("LVDeployerV1");
-  // const lvDeployer = await LVDeployerV1.deploy();
-  // await lvDeployer.waitForDeployment();
-  // const lvDeployerAddr = await lvDeployer.getAddress();
+  // deploy LVDeployer
+  const LVDeployerV1 = await ethers.getContractFactory("LVDeployerV1");
+  const lvDeployer = await LVDeployerV1.deploy();
+  await lvDeployer.waitForDeployment();
+  const lvDeployerAddr = await lvDeployer.getAddress();
 
-  // console.log(`LVDeployer deployed to: ${lvDeployerAddr}`)
+  console.log(`LVDeployer deployed to: ${lvDeployerAddr}`)
 
-  // // deploy LVFactory
-  // const LVFactoryV1 = await ethers.getContractFactory("LendingVaultV1Factory");
-  // const lvFactoryV1 = await LVFactoryV1.deploy(owner.address, reserveAdmin, lvDeployerAddr);
-  // await lvFactoryV1.waitForDeployment();
-  // const lvFactoryAddr = await lvFactoryV1.getAddress();
+  // deploy LVFactory
+  const LVFactoryV1 = await ethers.getContractFactory("LendingVaultV1Factory");
+  const lvFactoryV1 = await LVFactoryV1.deploy(owner.address, reserveAdmin, lvDeployerAddr);
+  await lvFactoryV1.waitForDeployment();
+  const lvFactoryAddr = await lvFactoryV1.getAddress();
 
-  // console.log(`LVFactory deployed to: ${lvFactoryAddr}`)
+  console.log(`LVFactory deployed to: ${lvFactoryAddr}`)
 
   let tx;
-  const lvFactoryV1 = await ethers.getContractAt("LendingVaultV1Factory", "0xaEA22Da48d4c15Ab10B8B7ad529c189ec9Ae24fE")
+  // const lvFactoryV1 = await ethers.getContractAt("LendingVaultV1Factory", "0xaEA22Da48d4c15Ab10B8B7ad529c189ec9Ae24fE")
   for (const underlying of underlyings) {
     tx = await lvFactoryV1.createVault(underlying.address, underlying.name, underlying.symbol);
     await tx.wait();
